891ceb556d812c96d82aa071576b47295db23e31
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-02 21:57:26 +0530

    ADD: VALIDATION OF MAIL ID

--- Regex.java
+++ Regex.java
@@ -1,25 +1,34 @@
 package Regex;
 
 import java.util.Scanner;
-import java.util.regex.*;
-;
+import java.util.regex.*;;
 
-public class Regex 
-{
+public class Regex {
 	public static String Name(String Name) {
 		Pattern name = Pattern.compile("^[A-Z][a-z]{2,}$");
 		Matcher checkName = name.matcher(Name);
 		if (checkName.find()) {
-			System.out.println("THANK YOU FOR ENTERING VALID FIRST NAME "); 
+			System.out.println("THANK YOU FOR ENTERING VALID FIRST NAME ");
 		} else {
 			System.err.println("YOU HAVE ENTERED INVALID FIRST NAME ");
 		}
 		return Name;
 	}
-    public static void main( String[] args )
-    {
-        System.out.println( "WELCOME REGEX BASED PROBLEMS" );
-    	Regex CheckName = new Regex();
+
+	public static String Email(String mail) {
+		Pattern email = Pattern.compile("^[a-zA-Z0-9]+([.][0-9a-zA-z])+@[a-z]+.[a-z]{2,3}$");
+		Matcher checkmail = email.matcher(mail);
+		if (checkmail.find()) {
+			System.out.println("THANK YOU FOR ENTERING VALID MAIL ID ");
+		} else {
+			System.err.println("YOU HAVE ENTERED INVALID MAIL ID ");
+		}
+		return mail;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME REGEX BASED PROBLEMS");
+		Regex CheckName = new Regex();
 		System.out.println("USER REGISTRATION");
 		Scanner firstName = new Scanner(System.in);
 		System.out.println("ENTER THE FIRST NAME: ");
@@ -28,8 +37,11 @@ public class Regex
 		Scanner lastName = new Scanner(System.in);
 		System.out.println("ENTER THE LAST NAME: ");
 		String lname = lastName.nextLine();
-        CheckName.Name(lname);
-      
-        	
+		CheckName.Name(lname);
+		Scanner mail = new Scanner(System.in);
+		System.out.println("ENTER THE MAIL ID: ");
+		String email = mail.nextLine();
+		CheckName.Email(email);
+
 	}
 }
