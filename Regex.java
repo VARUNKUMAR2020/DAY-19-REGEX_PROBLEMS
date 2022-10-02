5b35e9ef9888d85c7571b37027e668269e8f22fb
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-02 21:21:21 +0530

    ADD: VALIDATION OF FIRST NAME

--- Regex.java
+++ Regex.java
@@ -1,9 +1,31 @@
 package Regex;
 
+import java.util.Scanner;
+import java.util.regex.*;
+;
+
 public class Regex 
 {
+	public static String FirstName(String fName) {
+		Pattern name = Pattern.compile("^[A-Z][a-z]{2,}$");
+		Matcher checkName = name.matcher(fName);
+		if (checkName.find()) {
+			System.out.println("THANK YOU FOR ENTERING VALID FIRST NAME "); 
+		} else {
+			System.err.println("YOU HAVE ENTERED INVALID FIRST NAME ");
+		}
+		return fName;
+	}
     public static void main( String[] args )
     {
         System.out.println( "WELCOME REGEX BASED PROBLEMS" );
-    }
+    	Regex CheckName = new Regex();
+		System.out.println("USER REGISTRATION");
+		Scanner firstName = new Scanner(System.in);
+		System.out.println("ENTER THE FIRST NAME: ");
+		String Name = firstName.nextLine();
+		CheckName.FirstName(Name);
+      
+        	
+	}
 }
